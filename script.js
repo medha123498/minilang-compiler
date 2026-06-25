e6a5cf8305a293ac4cca66cc99cbabe9595243b1
medha123498 <nusratjahanmedha9@gmail.com>
2026-06-25 15:58:47 +0600

    Refactor compile function by removing syntax check
    Removed code that checks for 'print' in the input.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function compile(){
 
-let code=document.getElementById("code").value;
-
-if(code.includes("print"))
-{
-document.getElementById("result").innerHTML=
+document.getElementById("result").innerHTML =
 "✅ Compilation Successful";
-}
-else
-{
-document.getElementById("result").innerHTML=
-"❌ Syntax Error";
-}
 
 }
